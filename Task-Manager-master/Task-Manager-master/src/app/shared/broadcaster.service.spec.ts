2bf59935077d4e3bc79dd62ce4fa102a3c6603f6
hulya6525 <164415482+hulya6525@users.noreply.github.com>
2025-06-13 18:38:45 +0300

    Update broadcaster.service.spec.ts

--- Task-Manager-master/Task-Manager-master/src/app/shared/broadcaster.service.spec.ts
+++ Task-Manager-master/Task-Manager-master/src/app/shared/broadcaster.service.spec.ts
@@ -1,15 +1,21 @@
 import { TestBed } from '@angular/core/testing';
-
 import { BroadcasterService } from './broadcaster.service';
 
 describe('BroadcasterService', () => {
   let service: BroadcasterService;
 
+  // Her testten önce çalışacak setup bloğu
   beforeEach(() => {
+    // Test ortamı için gerekli modül yapılandırması
     TestBed.configureTestingModule({});
+
+    // Servisin test ortamında enjekte edilmesi
     service = TestBed.inject(BroadcasterService);
   });
 
+  /**
+   * Servisin başarıyla oluşturulup oluşturulmadığını test eder.
+   */
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
