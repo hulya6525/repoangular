d455f7f7c96cb972cd8b3e2598cdba6f8ba807ad
hulya6525 <164415482+hulya6525@users.noreply.github.com>
2025-06-13 18:26:08 +0300

    Update not-found.component.spec.ts

--- Task-Manager-master/Task-Manager-master/src/app/not-found/not-found.component.spec.ts
+++ Task-Manager-master/Task-Manager-master/src/app/not-found/not-found.component.spec.ts
@@ -3,20 +3,26 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NotFoundComponent } from './not-found.component';
 
 describe('NotFoundComponent', () => {
-  let component: NotFoundComponent;
-  let fixture: ComponentFixture<NotFoundComponent>;
+  let component: NotFoundComponent;               // Test edilecek bileşen örneği
+  let fixture: ComponentFixture<NotFoundComponent>; // Bileşenin test ortamı (DOM + Component)
 
+  // Her testten önce çalışacak hazırlık fonksiyonu
   beforeEach(async () => {
+    // Test modülünü yapılandır ve bileşeni import et
     await TestBed.configureTestingModule({
       imports: [NotFoundComponent],
     }).compileComponents();
 
+    // Bileşen örneği oluştur
     fixture = TestBed.createComponent(NotFoundComponent);
     component = fixture.componentInstance;
+
+    // Angular değişiklik algılamayı tetikle (template güncelle)
     fixture.detectChanges();
   });
 
+  // Bileşenin başarıyla oluşturulup oluşturulmadığını test eden birim testi
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(component).toBeTruthy();  // component nesnesinin varlığını doğrular
   });
 });
