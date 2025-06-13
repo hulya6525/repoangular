b09feb046a2c2edad14b17b8da0104eed555dbf5
hulya6525 <164415482+hulya6525@users.noreply.github.com>
2025-06-13 18:32:29 +0300

    Update auth.guard.ts

--- Task-Manager-master/Task-Manager-master/src/app/shared/auth.guard.ts
+++ Task-Manager-master/Task-Manager-master/src/app/shared/auth.guard.ts
@@ -1,8 +1,12 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 
+// authGuard: Kullanıcının yetkilendirilip yetkilendirilmediğini kontrol eden CanActivate fonksiyonu
 export const authGuard: CanActivateFn = (route, state) => {
+  // Router servisini injection yoluyla al
   const router = inject(Router);
 
+  // localStorage'da 'user' anahtarı varsa (kullanıcı oturumu açıksa) true döndür (erişim izinli)
+  // Aksi halde kullanıcı giriş sayfasına yönlendir ve navigate() çağrısının sonucunu döndür
   return localStorage.getItem('user') ? true : router.navigate(['/login']);
 };
