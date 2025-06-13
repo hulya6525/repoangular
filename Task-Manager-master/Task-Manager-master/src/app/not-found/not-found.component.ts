d7eefb8ddadee3ababc9aa2624e0f6e7d627aba3
hulya6525 <164415482+hulya6525@users.noreply.github.com>
2025-06-13 18:27:33 +0300

    Update not-found.component.ts

--- Task-Manager-master/Task-Manager-master/src/app/not-found/not-found.component.ts
+++ Task-Manager-master/Task-Manager-master/src/app/not-found/not-found.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 
+// Angular bileşen tanımı
 @Component({
-  selector: 'app-not-found',
-  standalone: true,
-  imports: [],
-  templateUrl: './not-found.component.html',
-  styleUrl: './not-found.component.scss'
+  selector: 'app-not-found',   // Bu bileşenin HTML'de kullanılacağı etiket adı
+  standalone: true,            // Bağımsız bileşen olduğunu belirtir 
+  imports: [],                 // Eğer başka modül/bileşen kullanılacaksa burada listelenir
+  templateUrl: './not-found.component.html',  // Bileşenin HTML şablon dosyası yolu
+  styleUrl: './not-found.component.scss'      // Bileşenin stil dosyası yolu
 })
 export class NotFoundComponent {
-
+  // Bileşenle ilgili işlevsellik burada yazılır
+  // Şu anda boş, sadece statik içerik gösteriyor
 }
